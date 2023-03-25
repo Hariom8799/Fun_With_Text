fe4c114b0a357ec91eb412c056a9d8a54bafedaa
Hariom <hariomkumar01868@gmail.com>
2023-03-25 23:44:21 +0530

    routers to a pages added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
 import React,{useState} from 'react';
 import './App.css';
+import Alert from './component/Alert';
 import Navbar from './component/Navbar';
 import TextArea from './component/TextArea';
+import Feedback from './component/Feedback';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alertText, setAlertText] = useState(null);
+
+  const alert=(message,type)=>{
+    setAlertText({
+      message : message,
+      type : type
+    });  
+    setTimeout(() => {
+      setAlertText(null);
+    }, 3000);
+  }
+
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor='#35373b';
+      alert("Dark Mode Enabled","success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor='white';
+      alert("Light Mode Enabled","success");
     }
   }
 
   return (
     <>
-        <Navbar title="FunWithText" aboutText="About Us" theme={mode} toggleMode={toggleMode} />
-      <div className="container" >
-        <TextArea theme={mode}/>
-      </div>
+    <Router>
+    <Navbar title="FunWithText" theme={mode} toggleMode={toggleMode} />
+    <Alert alert={alertText}/>
+      <Routes>
+        <Route path="/" element={<TextArea theme={mode} alert={alert}/>} />
+        <Route path="/Feedback" element={<Feedback/>} /> 
+      </Routes>
+    </Router>
       
     </>
   );
--- src/component/TextArea.js
+++ src/component/TextArea.js
@@ -6,59 +6,62 @@ export default function TextArea(props) {
 // ------------------btnFunctions----------------------------------------------
     const upbtn = ()=>{
         let newtext = text.toUpperCase()
-        setResptext(newtext)
+        setResptext(newtext);
+        props.alert("Converted to uppercase","success")
     }
     const lowerbtn = ()=>{
         let newtext = text.toLowerCase()
         setResptext(newtext)
+        props.alert("Converted to lowercase","success")
     }
-    const cursivebtn = ()=>{
-        // let newtext = text.toUpperCase()
+    const remSpace = ()=>{
+        let newText = text.replace(" ","")
+        setResptext(newText);
+        props.alert("Spaces removed successfully", "success")
     
     }
     const resetbtn = ()=>{
         settext("")
-        setResptext("")   
+        setResptext("")
+        props.alert("Text box reset ","success")   
     }
 
     const copybtn = ()=>{
-        console.log("upbtn clicked")
-        let newtext = text.toUpperCase()
-        settext(newtext)
+        navigator.clipboard.writeText(text)
+        props.alert("Text copied ","success")
     }
     const changing = (event)=>{
         settext(event.target.value)
     }
 
-// -----------------------------btn style--------------------------------------
-// const btnStyle={
-//     border:'1px solid crimson',
-//     backgroundColor:'transparent'
-// }
   return (
-    <> 
+    
+    <div className="container">
+
         <div className="mb-3">
-            <h1 style={{color:(props.theme==='dark'?'crimson':'black')}} className='mt-3 text-center'>Enter Text Below: </h1>
+            <h1 style={{color:(props.theme==='dark'?'white':'black')}} className='mt-3 text-center'>Enter Text Below: </h1>
             <textarea style={{backgroundColor:(props.theme==='dark'?'#35373b':'white'),color:(props.theme==='dark'?'white':'black')}} className="form-control" id="exampleFormControlTextarea1" rows="10" value={text} onChange={changing}></textarea>
             <div className="text-center my-3">
-                <button  className={` btn  ${props.theme==='light'?'btn-outline-success':'btnhover'} my-3 mx-1`} onClick={upbtn} >Convert To UpperCase</button>
-                <button className={` btn  ${props.theme==='light'?'btn-outline-success':'btnhover'} my-3 mx-1`} onClick={lowerbtn}>Convert To LowerCase</button>
-                <button className={` btn  ${props.theme==='light'?'btn-outline-success':'btnhover'} my-3 mx-1`}  onClick={cursivebtn}>Convert To Cursive</button>
-                <button className={` btn  ${props.theme==='light'?'btn-outline-success':'btnhover'} my-3 mx-1`} onClick={resetbtn}>Reset</button>
-                <button className={` btn  ${props.theme==='light'?'btn-outline-success':'btnhover'} my-3 mx-1`} onClick={copybtn}>Copy To Clipboard</button>
+                <button disabled={text.length===0} className={` btn  ${props.theme==='light'?'btn-outline-success':'btnhover'} my-3 mx-1 my-1`} onClick={upbtn} >Convert To UpperCase</button>
+                <button disabled={text.length===0} className={` btn  ${props.theme==='light'?'btn-outline-success':'btnhover'} my-3 mx-1 my-1`} onClick={lowerbtn}>Convert To LowerCase</button>
+                <button disabled={text.length===0} className={` btn  ${props.theme==='light'?'btn-outline-success':'btnhover'} my-3 mx-1 my-1`}  onClick={remSpace}>Remove Space</button>
+                <button disabled={text.length===0} className={` btn  ${props.theme==='light'?'btn-outline-success':'btnhover'} my-3 mx-1 my-1`} onClick={resetbtn}>Reset</button>
+                <button disabled={text.length===0} className={` btn  ${props.theme==='light'?'btn-outline-success':'btnhover'} my-3 mx-1 my-1`} onClick={copybtn}>Copy To Clipboard</button>
             </div>
-            <h1 style={{color:(props.theme==='dark'?'crimson':'black')}} className='text-center'>Response</h1>
+            <h1 style={{color:(props.theme==='dark'?'white':'black')}} className='text-center'>Response</h1>
             <textarea style={{backgroundColor:(props.theme==='dark'?'#35373b':'white')}} className="form-control my-3" id="exampleFormControlTextarea1" rows="10" value={respText} onChange={()=>{}} ></textarea>
               
         </div>
         <div style={{color:(props.theme==='dark'?'white':'black')}} className="container my-3 text-center">
-            <h2 style={{color:(props.theme==='dark'?'crimson':'black')}}>Your Text Summary: </h2>
-            <p>{text==="" ? 0 : text.split(" ").length} Words and {text.length} Character</p>
-            <p>{0.008 * (text==="" ? 0 : text.split(" ").length)} Minutes to read</p>
-            <h2 style={{color:(props.theme==='dark'?'crimson':'black')}}>Preview</h2>
+            <h2 >Your Text Summary: </h2>
+            <p>{ text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Words and {text.length} Character</p>
+            <p>{0.008 * ( text.split(/\s+/).filter((element)=>{return element.length!==0}).length)} Minutes to read</p>
+            <h2 >Preview</h2>
             <p>{text.length>0 ? text : "Enter something in textbox to preview here..."}</p>
         </div>
     
-    </>
+    </div>
+
+    
   )
 }
